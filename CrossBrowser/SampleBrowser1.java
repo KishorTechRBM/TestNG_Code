793f15fc15315d17bb93cca768b18b64989056dd
KishorTechRBM <123534039+KishorTechRBM@users.noreply.github.com>
2023-01-31 21:17:51 +0530

    Add files via upload

---
+++ CrossBrowser/SampleBrowser1.java
@@ -0,0 +1,39 @@
+package CrossBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SampleBrowser1 {
+	WebDriver driver = null;
+	String url = "https://www.google.com/";
+	String projectPath = System.getProperty("user.dir");
+
+	@Parameters("browserName")
+	@BeforeTest
+	public void setup(String browserName) {
+		System.out.println("Broser name is :" + browserName);
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("firefor")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/geckodriver.exe");
+			driver = new ChromeDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("internet_explorer")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/msedgedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		driver.get(url);
+		Thread.sleep(5000);
+	}
+}
